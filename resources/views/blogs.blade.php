3a316e7ba1831d6c40b47d3243c94c348fb3ec21
minsuwai <mswwai15@gmail.com>
2024-08-15 00:10:50 +0630

    feat: upadte database

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot name="title">
+        <title>All Blogs</title>
+    </x-slot>
+    @foreach ($blogs as $blog )
+    <div>
+        <h1><a href="blogs/{{$blog->id}}">
+                {{$blog->title}}
+            </a></h1>
+        <div>
+            <p>published at - {{$blog->created_at->diffForHumans()}}</p>
+            <p>{{$blog->intro}}</p>
+        </div>
+    </div>
+    @endforeach
+</x-layout>
