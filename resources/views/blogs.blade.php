a9aad60b25a88d7784af76a20cf2c070f765673f
minsuwai <mswwai15@gmail.com>
2024-08-21 22:40:39 +0630

    feat: bootstrap design integration

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,25 +1,14 @@
 <x-layout>
-    <x-slot name="title">
-        <title>All Blogs</title>
-    </x-slot>
-    @foreach($blogs as $blog)
-    <div>
-        <h1><a href="blogs/{{$blog->slug}}">
-                {{$blog->title}}
-            </a></h1>
-        <h4>Author - <a href="/users/{{$blog->author->username}}">{{$blog->author->name}}</a></h4>
-        <p>
-            <a href="/categories/{{$blog->category->slug}}">{{$blog->category->name}}</a>
-        </p>
-        <div>
-            <p>
-                published at -
-                {{$blog->created_at->diffForHumans()}}
-            </p>
-            <p>
-                {{$blog->intro}}
-            </p>
-        </div>
-    </div>
-    @endforeach
+
+    <!-- hero section -->
+    <x-hero></x-hero>
+
+    <!-- blogs section -->
+    <x-blog-section></x-blog-section>
+
+    <!-- subscribe new blogs -->
+    <x-subscribe></x-subscribe>
+
+    <!-- footer -->
+
 </x-layout>
