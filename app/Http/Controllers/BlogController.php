29d2d1b98ef3061893dc3fcb4c06c2005fd86d39
minsuwai <mswwai15@gmail.com>
2024-09-01 22:49:02 +0630

    feat: file naming conventions

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,24 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Blog;
-use App\Models\Category;
 
 class BlogController extends Controller
 {
-    function index()
+    public function index()
     {
-        // DB::listen(function ($query) {
-        //     logger($query->sql);
-        // });
-
-
-
-        return view('blogs', [
-            // eager load // lazy loading
-            // 'blogs' => Blog::with('category', 'author')->get() 
-
+        return view('blogs.index', [
             'blogs' => Blog::latest()
                 ->filter(request(['search', 'category', 'username']))
                 ->paginate(6)
@@ -29,27 +18,9 @@ class BlogController extends Controller
 
     public function show(Blog $blog)
     {
-        return view('blog', [
+        return view('blogs.show', [
             'blog' => $blog,
             'randomBlogs' => Blog::inRandomOrder()->take(3)->get()
         ]);
     }
-
-    // protected function getBlogs()
-    // {
-    //     return Blog::latest()->filter()->get();
-    //     // $query = Blog::latest();
-    //     // conditional query
-    //     // if (request('search')) {
-    //     //     $blogs = $blogs->where('title', 'like', '%' . request('search') . '%')
-    //     //         ->orWhere('body', 'like', '%' . request('search') . '%');
-    //     // }
-
-    //     // $query->when(request('search'), function ($query, $search) {
-    //     //     $query->where('title', 'like', '%' . $search . '%')
-    //     //         ->orWhere('body', 'like', '%' . $search . '%');
-    //     // });
-
-    //     // return $query->get();
-    // }
 }
--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,17 +1,15 @@
 <x-layout>
-
     <!-- single blog section -->
     <div class="container">
         <div class="row">
             <div class="col-md-6 mx-auto text-center">
-                <img
-                    src="https://creativecoder.s3.ap-southeast-1.amazonaws.com/blogs/GOLwpsybfhxH0DW8O6tRvpm4jCR6MZvDtGOFgjq0.jpg"
-                    class="card-img-top"
-                    alt="..." />
+                <img src="https://creativecoder.s3.ap-southeast-1.amazonaws.com/blogs/GOLwpsybfhxH0DW8O6tRvpm4jCR6MZvDtGOFgjq0.jpg"
+                    class="card-img-top" alt="..." />
                 <h3 class="my-3">{{$blog->title}}</h3>
                 <div>
                     <div>Author - <a href="/users/{{$blog->author->username}}">{{$blog->author->name}}</a></div>
-                    <div><a href="/categories/{{$blog->category->slug}}"><span class="badge bg-primary">{{$blog->category->name}}</span></a></div>
+                    <div><a href="/categories/{{$blog->category->slug}}"><span
+                                class="badge bg-primary">{{$blog->category->name}}</span></a></div>
                     <div class="text-secondary">{{$blog->created_at->diffForHumans()}}</div>
                 </div>
                 <p class="lh-md mt-3">
@@ -21,10 +19,6 @@
         </div>
     </div>
 
-    <!-- subscribe new blogs -->
-    <x-subscribe></x-subscribe>
-
-
-    <!-- Random Blog -->
+    <x-subscribe />
     <x-blogs_you_may_like_section :randomBlogs="$randomBlogs" />
 </x-layout>
